81c17c033d8b676f2edb42f46ab5d18789291c1b
xch88829051 <1152759491@qq.com>
2022-09-20 18:09:13 +0800

    Exercise 1.

--- exercise1.js
+++ exercise1.js
@@ -1,7 +1,7 @@
 fetch("https://jsonplaceholder.typicode.com/todos")
   .then(response => response.json())
   .then(json => {
-     const completed = null // Complete this code
+     const completed = todo.completed
      completed.forEach( (todo, index) => {
       console.log(`${todo.title} - ${todo.completed}`)
     })
